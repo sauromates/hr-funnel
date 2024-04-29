9e191f4db10b87cb3e5436b38fea727ab19840d0
Vsevolod Girenko <vs.girenko@mail.ru>
2024-04-29 12:41:28 +0300

    tests: configure nuxt test utils

--- pwa/nuxt.config.ts
+++ pwa/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@pinia/nuxt'],
+  modules: ['@nuxt/eslint', '@pinia/nuxt', '@nuxt/test-utils/module'],
   experimental: {
     // See https://github.com/api-platform/create-client/issues/382
     renderJsonPayloads: false,
