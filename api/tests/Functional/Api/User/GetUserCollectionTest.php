1d9da5b29212b0bed6347c1221ab4ca3884706c1
Vsevolod Girenko <vs.girenko@mail.ru>
2024-04-30 09:40:40 +0300

    tests: extend user collection test with pagination

--- api/tests/Functional/Api/User/GetUserCollectionTest.php
+++ api/tests/Functional/Api/User/GetUserCollectionTest.php
@@ -17,12 +17,14 @@ final class GetUserCollectionTest extends ApiTestCase
 
     public function testCanGetUsersCollection(): void
     {
-        $users = UserFactory::createMany(random_int(31, 99));
-        $usersCount = \count($users) + 1; // One user is created via fixtures load
+        // Create items for at least 3 page collection (hence the minimum of 91)
+        UserFactory::createMany(random_int(91, 199));
+
+        $usersCount = UserFactory::repository()->count();
         $perPage = 30;
 
         $response = self::createAuthenticatedClient()
-            ->request('GET', '/api/users')
+            ->request('GET', '/api/users?page=2') // Page query is used to check full pagination response
             ->toArray();
 
         $this->assertResponseIsSuccessful();
@@ -33,11 +35,12 @@ final class GetUserCollectionTest extends ApiTestCase
             '@type' => 'hydra:Collection',
             'hydra:totalItems' => $usersCount,
             'hydra:view' => [
-                '@id' => '/api/users?page=1',
+                '@id' => '/api/users?page=2',
                 '@type' => 'hydra:PartialCollectionView',
                 'hydra:first' => '/api/users?page=1',
                 'hydra:last' => '/api/users?page='.ceil($usersCount / $perPage),
-                'hydra:next' => '/api/users?page=2',
+                'hydra:previous' => '/api/users?page=1',
+                'hydra:next' => '/api/users?page=3',
             ],
         ]);
         $this->assertCount($perPage, $response['hydra:member']);
