368884405f8a4612d8b08d6648af36c1af557c1e
Vsevolod Girenko <vs.girenko@mail.ru>
2024-04-30 17:42:45 +0300

    chore: restore app.vue

--- pwa/pages/index.vue
+++ pwa/pages/index.vue
@@ -1,5 +1,17 @@
+<script setup lang="ts">
+const { isAuthenticated, user, logout } = useAuthStore();
+</script>
+
 <template>
-  <div>
-    <NuxtPage />
-  </div>
+  <UCard>
+    <template #header>
+      <div v-if="user" class="flex items-center justify-between">
+        <h1>Welcome, {{ user.name }}</h1>
+        <UButton @click="logout">Exit</UButton>
+      </div>
+
+      <h1 v-else>Please login to use the system</h1>
+    </template>
+    <UButton v-if="!isAuthenticated" to="/login">Proceed to login</UButton>
+  </UCard>
 </template>
