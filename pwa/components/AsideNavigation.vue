ce968f7b820c8a112867142310bc0288b3af826c
Vsevolod Girenko <vs.girenko@mail.ru>
2024-05-02 00:09:21 +0300

    chore: create aside navigation component

---
+++ pwa/components/AsideNavigation.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { VerticalNavigationLink } from '#ui/types';
+
+const links: VerticalNavigationLink[] = [
+  {
+    label: 'Vacancies',
+  },
+  {
+    label: 'Campaigns',
+  },
+  {
+    label: 'Candidates',
+  },
+];
+</script>
+
+<template>
+  <nav class="flex flex-col gap-6">
+    <UButton v-for="(link, idx) in links" :key="idx" variant="link" :label="link.label" :padded="false" />
+  </nav>
+</template>
