3782cbcfb0a9577ad0b168806d8f0e788eabef98
Vsevolod Girenko <vs.girenko@mail.ru>
2024-05-02 00:10:20 +0300

    feat: create login modal

---
+++ pwa/components/LoginButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const loginModal = ref<boolean>(false);
+</script>
+
+<template>
+  <UButton variant="outline" @click="loginModal = true">Login</UButton>
+  <UModal v-model="loginModal">
+    <UCard>
+      <template #header>
+        <h1>Please login to use the system</h1>
+      </template>
+      <LoginForm />
+    </UCard>
+  </UModal>
+</template>
