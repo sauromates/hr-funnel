02030c74be949441702237a539d4a6fa25f8663f
Vsevolod Girenko <69035880+sauromates@users.noreply.github.com>
2024-05-01 16:18:02 +0300

    JWT refresh tokens (#3)
    * chore: install jwt refresh token symfony bundle
    
    * chore: create refresh token entity
    
    * feat: configure refresh token generation and validation
    
    * fix: delete jwt cookie on logout
    
    * chore: create http status codes enum
    
    * refactor: extend and rename token storage composable
    
    * refactor!: create api platform nuxt plugin
    
    * refactor: simplify auth store actions
    
    * build: switch from pnpm to npm

--- pwa/composables/useApi.ts
+++ pwa/composables/useApi.ts
@@ -1,59 +1,9 @@
 import type { UseFetchOptions } from '#app';
-import setCookie from 'set-cookie-parser';
+import { defu } from 'defu';
 
-export async function useApi<T>(path: string, options: UseFetchOptions<T>) {
-  const event = useRequestEvent();
-  const config = useRuntimeConfig(event);
+export async function useApi<T>(path: string, options?: UseFetchOptions<T>) {
+  const { $apiClient, $defaultClientOptions } = useNuxtApp();
+  const params = defu($defaultClientOptions, options) as UseFetchOptions<T>;
 
-  const response = await useFetch(path, {
-    baseURL: config.public.baseURL,
-
-    mode: 'cors',
-
-    headers: {
-      Accept: 'application/ld+json',
-    },
-
-    onRequest({ options }) {
-      if (import.meta.server) {
-        // Request from the server should be directed to Docker service
-        // instead of public URL. Keep it for SSR to work.
-        options.baseURL = ENTRYPOINT; // Entrypoint defaults to `http://php`
-
-        const headers = options.headers;
-        const clientCookies = useRequestHeaders(['cookie']);
-
-        options.headers = {
-          ...headers,
-          ...(clientCookies.cookie && clientCookies),
-        };
-      }
-    },
-
-    onResponse({ response }): void {
-      if (import.meta.server) {
-        const rawCookies: string[] = response.headers.getSetCookie();
-        const cookies = setCookie.parse(rawCookies);
-
-        if (cookies.length > 0 && event !== undefined) {
-          for (const cookie of cookies) {
-            appendHeader(event, cookie.name, cookie.value);
-          }
-        }
-      }
-    },
-
-    async onResponseError({ response }) {
-      const data = response._data;
-      throw createError({
-        message: data['hydra:description'] || data.message,
-        statusCode: response.status,
-        statusMessage: response.statusText,
-      });
-    },
-
-    ...options,
-  });
-
-  return response;
+  return useFetch(path, { ...params, $fetch: $apiClient });
 }
