d26f67b1896b408650545ddddbfff1ad25a3c96f
Vsevolod Girenko <vs.girenko@mail.ru>
2024-05-04 16:38:22 +0300

    feat: create default footer component

---
+++ pwa/components/DefaultFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="w-full px-2 sm:px-4 lg:px-12 py-4">
+    <slot>
+      <UButton
+        variant="link"
+        color="gray"
+        size="xs"
+        icon="i-mdi-github"
+        to="https://github.com/sauromates/hr-funnel"
+        target="_blank"
+        :padded="false"
+      >
+        {{ `Published under MIT License &copy; ${new Date().getFullYear()}` }}
+      </UButton>
+    </slot>
+  </footer>
+</template>
