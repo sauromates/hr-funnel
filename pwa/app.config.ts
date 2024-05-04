f6d5e8e7d4e46f026b5450a2bafa9f88f327816e
Vsevolod Girenko <vs.girenko@mail.ru>
2024-05-04 16:34:58 +0300

    chore: override nuxt/ui container props

--- pwa/app.config.ts
+++ pwa/app.config.ts
@@ -2,5 +2,9 @@ export default defineAppConfig({
   ui: {
     primary: 'violet',
     gray: 'cool',
+    container: {
+      constrained: 'max-w-screen',
+      padding: 'px-2 sm:px-4 lg:px-12',
+    },
   },
 });
