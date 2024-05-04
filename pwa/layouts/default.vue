621416126212678d959cf22e5cf021f9224c2231
Vsevolod Girenko <vs.girenko@mail.ru>
2024-05-04 16:39:09 +0300

    refactor: simplify default layout

--- pwa/layouts/default.vue
+++ pwa/layouts/default.vue
@@ -1,19 +1,18 @@
 <template>
-  <div class="flex flex-col gap-4 min-h-screen">
-    <BaseHeader />
-    <div class="flex gap-12 container mx-auto my-6 flex-1">
-      <aside class="flex flex-col gap-4 basis-1/6">
-        <AsideNavigation />
-      </aside>
-      <main class="basis-5/6 flex flex-col items-start">
+  <UContainer :ui="{ base: 'flex flex-col gap-4 min-h-screen antialiased', constrained: '!px-0' }">
+    <DefaultHeader />
+    <UContainer id="container" :ui="{ base: 'flex gap-12 w-full grow' }">
+      <NavigationAside class="w-1/6 p-4 pl-0 hidden md:block" />
+      <main class="flex flex-col items-start grow p-4">
         <slot />
       </main>
-    </div>
-  </div>
+    </UContainer>
+    <DefaultFooter />
+  </UContainer>
 </template>
 
 <style>
 body {
-  @apply bg-gray-50 dark:bg-gray-900 text-gray-700 dark:text-gray-300 antialiased;
+  @apply bg-gray-50 dark:bg-gray-900 text-gray-700 dark:text-gray-300;
 }
 </style>
