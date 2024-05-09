c5ee7d6651ff69ac8a0384dbd583072ea18449d2
Vsevolod Girenko <vs.girenko@mail.ru>
2024-05-09 16:57:08 +0300

    chore: mark validator as psalm-api

--- api/src/Validator/IsListValidator.php
+++ api/src/Validator/IsListValidator.php
@@ -12,6 +12,8 @@ use Symfony\Component\Validator\Exception\UnexpectedValueException;
 /**
  * Validates that provided value is a list of specified type.
  *
+ * @psalm-api
+ *
  * @psalm-suppress UnimplementedInterfaceMethod Error due to extending framework base class
  */
 final class IsListValidator extends ConstraintValidator
