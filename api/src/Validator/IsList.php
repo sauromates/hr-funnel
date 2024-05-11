fbe4aff4f9a6eaf4b49fa95b4495f0c8008b5e1b
Vsevolod Girenko <vs.girenko@mail.ru>
2024-05-11 09:38:51 +0300

    fix: allow to use constraint with other assertions

--- api/src/Validator/IsList.php
+++ api/src/Validator/IsList.php
@@ -10,20 +10,24 @@ use Symfony\Component\Validator\Constraint;
 /**
  * Provides a shortcut constraint for arrays of single scalar type (lists).
  *
+ * @Annotation
+ * @Target({"PROPERTY", "METHOD", "ANNOTATION"})
+ *
  * @psalm-suppress PropertyNotSetInConstructor Error due to extending framework base class
  */
-#[\Attribute]
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
 final class IsList extends Constraint
 {
+    public string $message = 'Array is not a valid list: {{ value }} is not of type {{ type }}.';
+
     /**
-     * @param ScalarType|class-string  $type
-     * @param array<array-key, string> $groups
+     * @param ScalarType|class-string       $type
+     * @param array<array-key, string>|null $groups
      */
     #[HasNamedArguments]
     public function __construct(
         public ScalarType|string $type,
-        public string $message = 'Array is not a valid list: {{ value }} is not of type {{ type }}.',
-        array $groups = [],
+        ?array $groups = null,
         mixed $payload = null
     ) {
         parent::__construct([], $groups, $payload);
