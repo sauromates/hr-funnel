6802d40d841155e860a8a3121616ee0a10ec43cb
Vsevolod Girenko <vs.girenko@mail.ru>
2024-05-18 11:27:57 +0300

    chore: remove todo from factory template

--- api/tests/Factory/UserFactory.php
+++ api/tests/Factory/UserFactory.php
@@ -56,8 +56,6 @@ final class UserFactory extends ModelFactory
 
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
-     *
-     * @todo add your default values here
      */
     protected function getDefaults(): array
     {
